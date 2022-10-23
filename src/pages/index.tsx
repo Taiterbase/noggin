9c2e2b112bcebb87c5d0555e651af02275e662b6
Taite Nazifi <taitenazifi@gmail.com>
2022-10-22 17:08:17 -0700

    remove newline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ function HomePage(props: any) {
     return (
         <div>
             <p>
-
                 Home Page! Uneditable editor page with content created from the editor! Hooray!
             </p>
         </div>

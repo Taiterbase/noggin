24c5b7446325a0cbd1907e684ec529c3d436f264
Taite Nazifi <taitenazifi@gmail.com>
2022-11-13 13:53:35 -0700

    multi-line truncating and proper rerouting after delete

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -11,6 +11,7 @@ export default function NoteCard({ noteCard, selected, setSelected }) {
     const diff = TimeSince(noteCard.modified);
     const title = noteCard.preview.title;
     const content = noteCard.preview.content;
+    let deleted = false;
     let noteStyle = "";
     if (selected) {
         noteStyle = "border-l-red-500 bg-slate-200"
@@ -18,37 +19,41 @@ export default function NoteCard({ noteCard, selected, setSelected }) {
 
     return <div
         onClick={e => {
+            e.preventDefault();
+            let n = notes.notes.filter(n => n.id === id)[0];
+            console.log(n);
+            if (deleted) {
+                console.log("note is archived");
+                router.replace("/");
+                return
+            };
             setSelected(id);
-            console.log(noteCard)
             router.push({ pathname: "/[id]", query: { id: id }, });
         }}
         className={`max-h-24 max-w-56 flex flex-row justify-start border-l-4 whitespace-nowrap ${noteStyle} hover:border-l-red-500`}>
         <div className="flex flex-col justify-between pr-1 align-middle">
-            <div className="py-2 w-10">
+            <div className="flex-grow py-2 w-10">
                 <p className=" px-2 text-sm">{`${diff.diff}${diff.unit}`}</p>
             </div>
             {
-                // we should be able to delete a note just by right clicking the card
                 selected &&
-                <div className="flex-grow p-2 w-full h-4">
-                    <div onClick={() => {
-                        //delete the note
-                        notes.archiveNote({ id: id }).then(() => {
-                            router.push({ pathname: "/" })
-                        });
+                <div
+                    onClick={e => {
+                        e.preventDefault();
+                        deleted = true;
+                        notes.archiveNote({ id: id });
                     }}
-                    >
-                        X
-                    </div>
+                    className="flex-shrink w-full py-2 cursor-pointer text-center">
+                    X
                 </div>
             }
         </div>
-        <div className="py-2 w-60 max-w-[20rem] h-full border-b-[1px] overflow-hidden ">
-            <div className="h-8 w-full overflow-hidden">
-                <h2 className="text-lg overflow-hidden text-ellipsis font-normal before:content-none">{title}</h2>
+        <div className={`flex flex-col py-2 px-2 w-60 max-w-sm min-h-[5.5rem] border-b-[1px] overflow-hidden`}>
+            <div className="flex-grow">
+                <h2 className="truncate [display:-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical] whitespace-pre-wrap w-full text-lg font-semibold leading-5 before:content-none">{title}</h2>
             </div>
-            <div className="h-8 max-w-[20rem] overflow-hidden">
-                <p className="text-sm overflow-hidden text-ellipsis font-light">{content}</p>
+            <div className="flex-shrink">
+                <p className="text-sm truncate font-light">{content}</p>
             </div>
         </div>
     </div>

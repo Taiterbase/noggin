6b52c585b00f56530ac3a45dee22dccef20a1756
Taite Nazifi <taitenazifi@gmail.com>
2022-11-13 00:20:54 -0700

    rename note-toolbar to infobar

--- src/components/note-infobar.tsx
+++ src/components/note-infobar.tsx
@@ -1,6 +1,6 @@
 import { NoteCardResponse } from "models/note";
 
-export default function NoteToolbar(props: { noteCard: NoteCardResponse }) {
+export default function NoteInfobar(props: { noteCard: NoteCardResponse }) {
     if (props?.noteCard === null) {
         return <div className="fixed bottom-0 h-5 w-full flex flex-row flex-grow-0 bg-slate-300 z-30">
             {/* word count, spacing, styling drop-downs, line|column navigator, created date, history drop-down*/}
